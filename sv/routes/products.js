b868ebbab292268b375d668e31c38ddaeddad6c9
incolorate <negrupaul59@gmail.com>
2023-07-13 14:24:27 +0300

    Products correctly load from cache

--- sv/routes/products.js
+++ sv/routes/products.js
@@ -1,15 +1,25 @@
 import express from "express";
 import checkAuth from "../middlewares/checkAuth.js";
 import Product from "../models/productSchema.js";
+import client from "../client.js";
 
 const router = express.Router();
 
 router.get("/products", async (req, res) => {
-  try {
-    const allProducts = await Product.find();
-    res.status(200).json(allProducts);
-  } catch (error) {
-    res.status(500).json(error);
+  const cachedProducts = await client.get("products");
+  console.log(cachedProducts);
+  if (cachedProducts) {
+    res.status(200).json(JSON.parse(cachedProducts));
+    console.log("Products loaded from cache");
+  } else {
+    try {
+      const allProducts = await Product.find();
+      console.log("Loading products into cache... ");
+      client.set("products", JSON.stringify(allProducts));
+      res.status(200).json(allProducts);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
 });
 
