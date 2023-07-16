e141d8f500229926abd6bc3ad3ab9d7cb5fdab8d
incolorate <negrupaul59@gmail.com>
2023-07-16 13:50:22 +0300

    Added comments

--- ImportExport/Python/app.py
+++ ImportExport/Python/app.py
@@ -2,20 +2,22 @@ from pymongo import MongoClient
 import csv
 import time
 
+# Connect to db/define collection name
 mongo_uri = "mongodb+srv://incolorate:zWlXI0rS9iGVyT55@neobyteday1.zsgcc2w.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(mongo_uri)
-
 db = client["neobyteday4"]
 collection = db["Import"]
 
+# Measure performance
 start = time.time()
 
+# Define the file and the headers
 file = "./file.csv"
 headers = ["Customer Id", "First Name", "Last Name",
            "Company", "City", "Country", "Phone 1"]
 documents = []
 
-
+# Open file in read mode and "push" each row to documents
 with open(file, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
@@ -26,5 +28,6 @@ with open(file, "r") as csv_file:
 if documents:
     collection.insert_many(documents)
 
+# End timer print time
 end = time.time()
 print(end - start)
