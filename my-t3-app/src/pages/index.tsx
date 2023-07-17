4bf9d6d28de8958e4eb768989f580afe89304f49
incolorate <negrupaul59@gmail.com>
2023-07-17 14:27:12 +0300

    Can filter on each column

--- my-t3-app/src/pages/index.tsx
+++ my-t3-app/src/pages/index.tsx
@@ -13,6 +13,7 @@ type Customer = {
 
 export default function Home() {
   const [search, setSearch] = useState("");
+  const [filterBy, setFilterBy] = useState("First_Name");
 
   const allData = api.example.getAll.useQuery();
 
@@ -21,14 +22,23 @@ export default function Home() {
     console.log(search);
   };
 
+  const handleSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterBy(e.target.value);
+    console.log(filterBy);
+  };
+
   if (!allData.isFetched) {
     return <h1>Loading...</h1>;
   }
 
-  const filteredData: Customer[] =
-    (allData.data?.filter((object: Customer) =>
-      object.First_Name.toLowerCase().includes(search.toLowerCase())
-    ) as Customer[]) ?? [];
+  console.log(allData.data[0]);
+
+  const filteredData: Customer[] = allData
+    ? (allData.data?.filter((object: Customer) =>
+        object[filterBy].toLowerCase().includes(search.toLowerCase())
+      ) as Customer[]) ?? []
+    : "hello";
+
   return (
     <>
       <Head>
@@ -42,13 +52,28 @@ export default function Home() {
           className="w-44 text-black"
           value={search}
           onChange={handleSearch}
-        ></input>
+        />
+        <select
+          name="cars"
+          id="cars"
+          className="text-black"
+          onChange={handleSelect}
+        >
+          <option value="First_Name">First name</option>
+          <option value="Last_Name">Last Name</option>
+          <option value="Company">Company</option>
+          <option value="City">City</option>
+          <option value="Country">Country</option>
+          <option value="Customer_Id">Customer ID</option>
+          <option value="Phone_1">Phone</option>
+        </select>
         <table>
           <thead>
             <tr>
               <th>First Name</th>
               <th>Last Name</th>
               <th>Company</th>
+              <th>City</th>
               <th>Country</th>
               <th>Phone</th>
             </tr>
@@ -60,6 +85,7 @@ export default function Home() {
                   <td>{object.First_Name}</td>
                   <td>{object.Last_Name}</td>
                   <td>{object.Company}</td>
+                  <td>{object.City}</td>
                   <td>{object.Country}</td>
                   <td>{object.Phone_1}</td>
                 </tr>
