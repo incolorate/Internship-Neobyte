b6751e554d07f7ef59966cbbc40ddbecd144bc73
incolorate <negrupaul59@gmail.com>
2023-07-19 17:59:58 +0300

    Check pass/user  correct

--- my-t3-app/src/pages/login.tsx
+++ my-t3-app/src/pages/login.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/dist/client/link";
 import { api } from "~/utils/api";
+import LoginComponent from "~/components/LoginComponent";
 
 type User = {
   email: string;
@@ -11,155 +12,35 @@ type User = {
 };
 
 export default function SignInForm() {
-  const [userFrom, setUserForm] = useState<User>();
+  const [userFrom, setUserForm] = useState<User>({ email: "", password: "" });
+  const [pendingVerification, setPendingVerification] =
+    useState<boolean>(false);
 
-  const [logedIn, setLogedIn] = useState(false);
-  const [validationValue, setValidationValue] = useState("");
-  const user = useUser();
-  const check = api.example.handleLogin.useMutation();
-  const { isLoaded, signIn, setActive } = useSignIn();
-  const router = useRouter();
+  const login = api.example.handleLogin.useMutation();
 
-  const submit = (e) => {
-    e.preventDefault();
-    check.mutate({ email, password });
-    setLogedIn(true);
+  const handleFormChange = (e) => {
+    setUserForm({ ...userFrom, [e.target.name]: e.target.value });
   };
 
-  const checkCode = async (e) => {
-    await signIn
-      .create({
-        identifier: email,
-        password,
-      })
-      .then((result) => {
-        if (result.status === "complete") {
-          console.log(result);
-          setActive({ session: result.createdSessionId });
-        } else {
-          console.log(result);
-        }
-      })
-      .catch((err) => console.error("error", err.errors[0].longMessage));
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      login.mutate({
+        email: userFrom.email,
+        password: userFrom.password,
+      });
+      console.log(login.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setPendingVerification(true);
   };
 
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to Neobyte
-          </h2>
-        </div>
-        {logedIn && (
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={checkCode}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Validation code
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="validation"
-                    name="validation"
-                    type="text"
-                    autoComplete="validation"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    onChange={(e) => setValidationValue(e.target.value)}
-                  />
-                  <button></button>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-          </div>
-        )}
-
-        {logedIn || (
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={submit}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    onChange={(e) =>
-                      setUserForm({ ...userFrom, email: e.target.value })
-                    }
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    onChange={(e) =>
-                      setUserForm({ ...userFrom, password: e.target.value })
-                    }
-                  />
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
-
-            <Link href="/signup">
-              <p className="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <p className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                  SignUp!
-                </p>
-              </p>
-            </Link>
-          </div>
+        {!pendingVerification && (
+          <LoginComponent handleFormChange={handleFormChange} />
         )}
       </div>
     </>
