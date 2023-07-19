3d5944290ebdefdfa84fc463a99c19da9084b766
incolorate <negrupaul59@gmail.com>
2023-07-19 17:58:00 +0300

    Moved login comp/validation comp from login page

---
+++ my-t3-app/src/components/LoginValidation.tsx
@@ -0,0 +1,37 @@
+export default function LoginValidation() {
+  return (
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form className="space-y-6" onSubmit={handleSubmit}>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Validation code
+          </label>
+          <div className="mt-2">
+            <input
+              id="validation"
+              name="validation"
+              type="text"
+              autoComplete="validation"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => setValidationValue(e.target.value)}
+            />
+            <button></button>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
