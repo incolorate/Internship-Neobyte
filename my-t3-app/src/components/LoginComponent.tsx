669ca2601ac7c53b16aecacd5ada771e5dae3e5a
incolorate <negrupaul59@gmail.com>
2023-07-19 18:22:49 +0300

    Save server validation code to state

--- my-t3-app/src/components/LoginComponent.tsx
+++ my-t3-app/src/components/LoginComponent.tsx
@@ -1,20 +1,10 @@
 import Link from "next/dist/client/link";
 
-export default function LoginComponent({ handleFormChange }) {
+export default function LoginComponent({ handleFormChange, handleFormSubmit }) {
   return (
     <>
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to Neobyte
-        </h2>
-      </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleFormSubmit}>
           <div>
             <label
               htmlFor="email"
--- my-t3-app/src/pages/login.tsx
+++ my-t3-app/src/pages/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSignIn } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { useUser } from "@clerk/nextjs";
@@ -15,6 +15,7 @@ export default function SignInForm() {
   const [userFrom, setUserForm] = useState<User>({ email: "", password: "" });
   const [pendingVerification, setPendingVerification] =
     useState<boolean>(false);
+  const [validationCode, setValidationCode] = useState<string>("");
 
   const login = api.example.handleLogin.useMutation();
 
@@ -22,25 +23,43 @@ export default function SignInForm() {
     setUserForm({ ...userFrom, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
       login.mutate({
         email: userFrom.email,
         password: userFrom.password,
       });
-      console.log(login.data);
     } catch (error) {
       console.log(error);
     }
+
     setPendingVerification(true);
   };
+
+  // Get official validation code
+  useEffect(() => {
+    setValidationCode(login.data);
+  }, [login]);
 
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to Neobyte
+          </h2>
+        </div>
         {!pendingVerification && (
-          <LoginComponent handleFormChange={handleFormChange} />
+          <LoginComponent
+            handleFormChange={handleFormChange}
+            handleFormSubmit={handleFormSubmit}
+          />
         )}
       </div>
     </>
