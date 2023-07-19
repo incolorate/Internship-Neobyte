58448ebd6121b1bdb1477fd6e99e5a3129b0c037
incolorate <negrupaul59@gmail.com>
2023-07-19 19:17:52 +0300

    Added props to validation component

--- my-t3-app/src/components/LoginValidation.tsx
+++ my-t3-app/src/components/LoginValidation.tsx
@@ -1,7 +1,11 @@
-export default function LoginValidation() {
+export default function LoginValidation({
+  handleUserValidationCode,
+  handleValidationSubmit,
+  invalidCode,
+}) {
   return (
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form className="space-y-6" onSubmit={handleSubmit}>
+      <form className="space-y-6" onSubmit={handleValidationSubmit}>
         <div>
           <label
             htmlFor="email"
@@ -17,18 +21,17 @@ export default function LoginValidation() {
               autoComplete="validation"
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              onChange={(e) => setValidationValue(e.target.value)}
+              onChange={(e) => handleUserValidationCode(e)}
             />
-            <button></button>
           </div>
         </div>
-
+        {invalidCode && <p className="text-red-500">Invalid code</p>}
         <div>
           <button
             type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Sign in
+            Validate
           </button>
         </div>
       </form>
