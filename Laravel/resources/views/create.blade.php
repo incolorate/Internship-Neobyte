068f2d8c9cc53c7034e938e8580621beb99ba6df
incolorate <negrupaul59@gmail.com>
2023-07-25 18:59:06 +0300

    Create product form using bootstrap

--- Laravel/resources/views/create.blade.php
+++ Laravel/resources/views/create.blade.php
@@ -1,18 +1,33 @@
 @extends('layouts.app')
 
-
 @section("title", "Add product")
 
-
 @section("content")
-    <form method="POST" action="{{route("products.store")}}">
-        @csrf
-        <label for="productName">Product name</label>
-        <input id="productName" name="name" type="text" placeholder="Product name">
-        <label for="productPrice">Product price</label>
-        <input id="productPrice" name="price" type="number" placeholder="Product price">
-        <label for="productStock">Product stock</label>
-        <input id="productStock" name="stock" type="number" placeholder="Product stock">
-        <button type="submit">Add product</button>
-    </form>
+  <form method="POST" action="{{ route('product.store') }}">
+    @method('POST')
+    @csrf
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" name="name" placeholder="Enter product name">
+      @error('name')
+        <p class="text-danger">{{ $message }}</p>
+      @enderror
+    </div>
+    <div class="form-group">
+      <label for="price">Price</label>
+      <input type="number" class="form-control" id="price" name="price" placeholder="Enter product price">
+      @error('price')
+        <p class="text-danger">{{ $message }}</p>
+      @enderror
+    </div>
+    <div class="form-group">
+      <label for="stock">Stock</label>
+      <input type="number" class="form-control" id="stock" name="stock" placeholder="Stock">
+      @error('stock')
+        <p class="text-danger">{{ $message }}</p>
+      @enderror
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 @endsection
