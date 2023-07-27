0a131a13da28b90a48829ce59430e0041d585ebb
incolorate <negrupaul59@gmail.com>
2023-07-27 14:08:01 +0300

    V2 still incorrect

--- NextT3/src/components/waterworld.ts
+++ NextT3/src/components/waterworld.ts
@@ -1,104 +1,116 @@
-export default function waterWorld(height, length, dominant) {
+export default function waterWorld(height, length) {
   const dummyBoard = {};
-  const generateBoard = (gameHeight, gameLength) => {
-    for (let i = 0; i < gameHeight; i++) {
+
+  const generateObject = (height, length) => {
+    for (i = 0; i < height; i++) {
       dummyBoard[i] = [];
-      for (let j = 0; j < gameLength; j++) {
-        //  surround the board with water
-        // first row case and first column case
-        if (i === 0 || j === 0) {
-          dummyBoard[i].push(0);
-          continue;
-        }
-        //  last column case
-        if (j === gameLength - 1) {
+      for (j = 0; j < length; j++) {
+        // first row/last row
+        if (i === 0 || i === height - 1) {
           dummyBoard[i].push(0);
           continue;
         }
 
-        // Last row
-        if (i === gameHeight - 1) {
-          dummyBoard[i].push(0);
+        // first column/ last column
+        if (j === 0 || j === length - 1) {
+          dummyBoard[i][j] = 0;
           continue;
         }
+        dummyBoard[i].push(-1);
+      }
+    }
+  };
+  generateObject(height, length);
 
-        // start from left let middle unchecked
-        if (i !== 0 && j < Math.floor(gameLength / 2)) {
-          const lookUp = dummyBoard[i - 1][j];
-          const possibleUpValues = [lookUp, lookUp - 1, lookUp + 1].filter(
-            (number) => number <= 5 && number >= 0
+  const generateBoard = (gameHeight, gameLength) => {
+    // don't check first row i=0...
+    for (let i = 1; i < gameHeight - 1; i++) {
+      // first row  do nothing
+      // first column and alst column do nothing
+      for (let j = 1; j < gameLength - 2; j++) {
+        if (j === Math.floor(gameLength / 2)) {
+          dummyBoard[i][j] = "middle";
+
+          const leftValue = dummyBoard[i][j - 1];
+          const rightValue = dummyBoard[i][j + 1];
+          const upValue = dummyBoard[i - 1][j];
+          const rightUpPossibilities = [
+            rightValue,
+            rightValue + 1,
+            rightValue - 1,
+          ].filter((number) => number >= 0 && number <= 5);
+          const upPossibilities = [upValue, upValue + 1, upValue - 1].filter(
+            (number) => number >= 0 && number <= 5
           );
-          const lookLeft = dummyBoard[i][j - 1];
-          const possibleLeftValues = [
-            lookLeft,
-            lookLeft + 1,
-            lookLeft - 1,
-          ].filter((number) => number <= 5 && number >= 0);
-          const currentPossibilities = possibleUpValues.filter((element) =>
-            possibleLeftValues.includes(element)
+          const rightVsUp = upPossibilities.filter((element) =>
+            rightUpPossibilities.includes(element)
           );
-
-          dummyBoard[i].push(
+          const leftPossibilities = [
+            leftValue,
+            leftValue + 1,
+            leftValue - 1,
+          ].filter((number) => number >= 0 && number <= 5);
+          const currentPossibilities = rightVsUp.filter((element) =>
+            leftPossibilities.includes(element)
+          );
+          dummyBoard[i][j] =
             currentPossibilities[
               Math.floor(Math.random() * currentPossibilities.length)
-            ]
-          );
-        }
-
-        // look at right part
-        if (i !== 0 && j > Math.floor(gameLength / 2)) {
-          const lookUp = dummyBoard[i - 1][j];
-          const lookRight = dummyBoard[i][gameLength - 1 - j];
-          const possibleUpValues = [lookUp, lookUp - 1, lookUp + 1].filter(
-            (number) => number <= 5 && number >= 0
-          );
-          const possibleRightValues = [
-            lookRight,
-            lookRight - 1,
-            lookRight + 1,
-          ].filter((number) => number <= 5 && number >= 0);
-          const currentPossibilities = possibleUpValues.filter((element) =>
-            possibleRightValues.includes(element)
-          );
-          dummyBoard[i].push(
-            currentPossibilities[
-              Math.floor(Math.random() * currentPossibilities.length)
-            ]
-          );
+            ];
           continue;
         }
 
-        // look at middle
-        if (i !== 0 && j === Math.floor(gameLength / 2)) {
-          // look left look right and up
-          const lookUp = dummyBoard[i - 1][j];
-          const lookRight = dummyBoard[i][gameLength - 1 - j];
-          const possibleUpValues = [lookUp, lookUp - 1, lookUp + 1].filter(
-            (number) => number <= 5 && number >= 0
+        if (
+          j !== Math.floor(gameLength / 2) &&
+          j <= Math.floor(gameLength / 2 - 2)
+        ) {
+          // handle left value
+          const upValue = dummyBoard[i - 1][j];
+          // Look left
+          const leftValue = dummyBoard[i][j - 1];
+          // get all Possibilities
+          const upPossibilities = [upValue, upValue + 1, upValue - 1].filter(
+            (number) => number >= 0 && number <= 5
           );
-          const possibleRightValues = [
-            lookRight,
-            lookRight - 1,
-            lookRight + 1,
-          ].filter((number) => number <= 5 && number >= 0);
-          // get up and right posibilities
-          const currentPossibilities = possibleUpValues.filter((element) =>
-            possibleRightValues.includes(element)
+
+          const leftPossibilities = [
+            leftValue,
+            leftValue + 1,
+            leftValue - 1,
+          ].filter((number) => number >= 0 && number <= 5);
+          const currentPossibilities = upPossibilities.filter((element) =>
+            leftPossibilities.includes(element)
           );
-          // Look left const lookLeft = dummyBoard[i][j-1]
-          const lookLeft = dummyBoard[i][j - 1];
-          const possibleLeftValues = [
-            lookLeft,
-            lookLeft + 1,
-            lookLeft - 1,
-          ].filter((number) => number <= 5 && number >= 0);
-          const actualPosib = currentPossibilities.filter((element) =>
-            possibleLeftValues.includes(element)
+          dummyBoard[i][j] =
+            currentPossibilities[
+              Math.floor(Math.random() * currentPossibilities.length)
+            ];
+
+          // handle right value
+          const previousRowLength = dummyBoard[i - 1].length;
+          const rightUp = dummyBoard[i - 1][previousRowLength - 1 - j];
+          const rightValue = dummyBoard[i][previousRowLength - j];
+
+          const rightUpPossibilities = [
+            rightUp,
+            rightUp + 1,
+            rightUp - 1,
+          ].filter((number) => number >= 0 && number <= 5);
+
+          const rightValuePossibilities = [
+            rightValue,
+            rightValue + 1,
+            rightValue - 1,
+          ].filter((number) => number >= 0 && number <= 5);
+
+          const rightPossibilities = rightUpPossibilities.filter((element) =>
+            rightValuePossibilities.includes(element)
           );
-          dummyBoard[i].push(
-            actualPosib[Math.floor(Math.random() * actualPosib.length)]
-          );
-          console.log("i was in middle");
+
+          dummyBoard[i][previousRowLength - 1 - j] =
+            rightValuePossibilities[
+              Math.floor(Math.random() * rightValuePossibilities.length)
+            ];
           continue;
         }
       }
