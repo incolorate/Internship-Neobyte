0e1d82fd83d7436b7b9cb86abe6334eb5e94cedd
incolorate <negrupaul59@gmail.com>
2023-08-04 18:39:42 +0300

    Added "class-variance-authority"

---
+++ LaravelVue/resources/js/CustomComponents/Button.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from "vue";
+import { cva } from "class-variance-authority";
+
+const props = defineProps({
+    intent: {
+        type: String,
+        validator: (type) => ["primary", "secondary"].includes(type),
+        default: "primary",
+    },
+});
+
+const buttonClass = computed(() => {
+    return cva("text-sm rounded-sm px-3 py-1", {
+        variants: {
+            intent: {
+                primary: "bg-slate-950 text-slate-100  hover:bg-slate-700",
+            },
+        },
+    });
+});
+</script>
+
+<template>
+    <button :class="buttonClass">
+        <slot />
+    </button>
+</template>
