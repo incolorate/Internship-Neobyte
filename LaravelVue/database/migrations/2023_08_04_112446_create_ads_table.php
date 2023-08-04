8bf5023e0bb90963185deb2213a128bd80c9fb08
incolorate <negrupaul59@gmail.com>
2023-08-04 14:47:24 +0300

    Ads schema created

---
+++ LaravelVue/database/migrations/2023_08_04_112446_create_ads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string("title")->nullable(false)->unique();
+            $table->string("description")->nullable();
+            $table->integer("price")->nullable();
+            $table->string("image")->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');    
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ads');
+    }
+};
