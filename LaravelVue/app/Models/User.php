77e17f8810ff4e1006e0660cb9257e4d25ca08d4
incolorate <negrupaul59@gmail.com>
2023-08-04 14:22:52 +0300

    Ad model and schema created-> relationship between ads and users created

--- LaravelVue/app/Models/User.php
+++ LaravelVue/app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function ad() 
+    {
+        return $this->hasMany(Ad::class);
+    }
 }
